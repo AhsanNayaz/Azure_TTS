327deef5cccdaf85409ef800ee690927343aabe3
Ahsan_admin <ahsan_admin@192.168.1.136>
2023-07-07 15:35:15 +0530

    ld

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-
 import azure.cognitiveservices.speech as speechsdk
 import os
 import streamlit as st
@@ -34,4 +33,14 @@ input_text = st.text_input("enter your text here....")
 button = st.button("submit")
 
 if button and input_text:
-    result = speech_synthesizer.speak_text_async(input_text).get()
+    speech_synthesis_result = speech_synthesizer.speak_text_async(input_text).get()
+    #result = speech_synthesizer.speak_text_async(input_text).get()
+    if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        print("Speech synthesized for text [{}]".format(input_text))
+    elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = speech_synthesis_result.cancellation_details
+        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            if cancellation_details.error_details:
+                print("Error details: {}".format(cancellation_details.error_details))
+                print("Did you set the speech resource key and region values?")
