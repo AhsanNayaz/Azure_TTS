3c7e4e15249ec128b2910a4ee1f06e4418d29413
Ahsan_admin <ahsan_admin@192.168.1.136>
2023-07-07 16:49:08 +0530

    f

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, render_template
 import azure.cognitiveservices.speech as speechsdk
 import os
+from pydub import AudioSegment
+from pydub.playback import play
 
 speech_key, service_region = os.environ["SPEECH_KEY"], os.environ["SPEECH_REGION"]
 speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
 
 # Set the voice name, refer to https://aka.ms/speech/voices/neural for full list.
 speech_config.speech_synthesis_voice_name = "en-US-AriaNeural"
-
+audio_config = speechsdk.audio.AudioOutputConfig(filename="file.wav")
 # Creates a speech synthesizer using the default speaker as audio output.
-speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
 app = Flask(__name__)
 
@@ -21,8 +23,10 @@ def main():
         # Unpickle classifier
         # Get values through input bars
         input_text = request.form.get("name")
-        speech_synthesis_result = speech_synthesizer.speak_text_async(input_text).get()
-        return render_template("index.html", output=speech_synthesis_result)
+        speech_synthesizer.speak_text_async(input_text)
+        song = AudioSegment.from_wav("file.wav")
+        play(song)
+        return render_template("index.html", output='done')
     return render_template("index.html")
 
 
